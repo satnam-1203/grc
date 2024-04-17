2badeadbb141be69848345c8825c312c31f02f0e
satnam-1203 <167192846+satnam-1203@users.noreply.github.com>
2024-04-17 12:57:25 +0530

    Update StudentDashboard.jsx

--- src/Pages/StudentDashboard.jsx
+++ src/Pages/StudentDashboard.jsx
@@ -1,3 +1,28 @@
+import '../App.css';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import TopHeader from "../Component/TopHeader";
+import Navigation from "../Component/Navigation";
+import Footer from "../Component/Footer";
+import Sidebar from "../Component/StudentSideMenu";
+import StudentProfile from '../Component/StudentProfile';
+import StudentDash from '../Component/StuDash';
+import StudentResult from '../Component/StudentResult';
+import StudentSyllabus from '../Component/studentSyllabus';
+import FeeNotification from './feeNotif';
+import Axios from 'axios';
+import { API_URL } from '../utils/config';
+
+function StudentDashboard() {
+    const [selectedTab, setSelectedTab] = useState("student_dashboard");
+    const navigate = useNavigate();
+
+    const handleTabChange = (tab) => {
+        setSelectedTab(tab);
+    };
+
+    Axios.defaults.withCredentials = true;
+
 useEffect(() => {
     Axios.get(`${API_URL}/getLoggedInStudentData`, { withCredentials: true })
         .then(response => {
@@ -25,3 +50,74 @@ useEffect(() => {
             navigate('/');
         });
 }, [navigate]);
+
+    return (
+        //  main container of body
+        <>
+            <TopHeader />
+            <Navigation />
+            {/* main webpage of contact us div */}
+            <div id="PageContainer">
+                <Sidebar onTabChange={handleTabChange} />
+                {/* student dashboard page */}
+                <div className="managementPage">
+                    {selectedTab === "student_dashboard" && (
+                        <div id="tabContent">
+                            <div id="tabData">
+                                <StudentDash />
+                            </div>
+
+                        </div>
+                    )}
+
+                    {/* div for sidebar */}
+                    {/* student profile */}
+                    {selectedTab === "student_profile" && (
+                        <div id="tabContent">
+                            <div id="tabData">
+                                <StudentProfile />
+                            </div>
+
+                        </div>
+                    )}
+
+                    {/* student result */}
+                    {selectedTab === "student_result" && (
+                        <div id="tabContent">
+                            <div id="tabData">
+                                <StudentResult />
+                            </div>
+
+                        </div>
+                    )}
+
+                    {/* student result */}
+                    {selectedTab === "student_notification" && (
+                        <div id="tabContent">
+                            <div id="tabData">
+                                <FeeNotification />
+                            </div>
+
+                        </div>
+                    )}
+
+                    {/* student result */}
+                    {selectedTab === "student_syllabus" && (
+                        <div id="tabContent">
+                            <div id="tabData">
+                                <StudentSyllabus />
+                            </div>
+
+                        </div>
+                    )}
+
+
+                </div>
+            </div>
+            <Footer />
+        </>
+
+    );
+}
+
+export default StudentDashboard;
