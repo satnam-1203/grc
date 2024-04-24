132c4c9685734abdd1de51963617fb73598b9fc8
satnam-1203 <167192846+satnam-1203@users.noreply.github.com>
2024-04-24 07:39:44 +0530

    Update Navigation.jsx

--- src/Component/Navigation.jsx
+++ src/Component/Navigation.jsx
@@ -1,20 +1,34 @@
+//Naviagtion.jsx
+
 import '../App.css';
 import React from "react";
 import { Link } from 'react-router-dom';
 
 function Navigation() {
+    const [showMenu, setShowMenu] = React.useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
     return (
         <>
             {/* navigation bar starts here */}
             <div id="navbar">
                 <div className='logo'>GRC Online Portal</div>
-                <div id='navMenu'>
-                    <ul>
-                        <li><Link className="link" to="/home">Home</Link></li>
-                        <li><Link className="link" to="/courses">Courses</Link></li>
-                        <li><Link className="link" to="/about">About</Link></li>
-                        <li><Link className="link" to="/Contact">Contact Us</Link></li>
-                    </ul>
+                <div className="menu-icon" onClick={toggleMenu}>
+                    <i className={showMenu ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+
+                <div id="showMenu">
+                    <div id='navMenu' className={showMenu ? 'show' : ''}>
+                        <ul>
+                            <li><Link className="link" to="/home">Home</Link></li>
+                            <li><Link className="link" to="/courses">Courses</Link></li>
+                            <li><Link className="link" to="/about">About</Link></li>
+                            <li><Link className="link" to="/Contact">Contact Us</Link></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             {/* nav bar ends here */}
